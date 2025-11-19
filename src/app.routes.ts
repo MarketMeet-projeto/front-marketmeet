64c718b9bc64813e4f84304fce9575295ab537ae
Aleksander Oliveira Piassalonga <aleksanderoliveira94@gmail.com>
2025-11-19 20:00:51 -0300

    att

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './app/guards/auth.guard';
+import { HttpBackend } from '@angular/common/http';
 
 export const routes: Routes = [
   {
